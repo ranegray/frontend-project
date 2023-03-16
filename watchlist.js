bd6e7cda273af2381bf2abc2fe60a4a4ec805fec
Rane Gray <ranegray830@gmail.com>
2023-03-16 17:56:52 +0100

    Finish functionality to remove from watchlist

--- watchlist.js
+++ watchlist.js
@@ -1,16 +1,26 @@
 if (localStorage.getItem("watchlist")) {
   let watchlist = localStorage.getItem("watchlist");
-  watchlist = JSON.parse(watchlist)
-
-  watchlist.forEach(movie => {
-      $(".movies").append(`<div class="movie-card">${movie}<div class="remove"></div></div>`);
-      $(".add-watch").remove()
-    });
-    console.log(watchlist);
+  watchlist = JSON.parse(watchlist);
+  for (let i = 0; i < watchlist.length; i++) {
+    let $movieCard = $(`<div class="movie-card">${watchlist[i]}</div>`);
+    let $remove = $("<div></div>")
+      .attr("id", i)
+      .addClass("remove")
+      .on("click", (event) => {
+        let removeTarget = event.target.parentElement;
+        let index = watchlist.indexOf(watchlist[i]);
+        removeTarget.remove();
+        watchlist.splice(index, 1)
+        localStorage.setItem("watchlist", JSON.stringify(watchlist));
+      });
+    $movieCard.append($remove);
+    $(".movies").append($movieCard);
+    $(".add-watch").remove();
+    console.log(watchlist[i])
+  }
 } else {
-    $('.movies').append('<h2>Nothing added yet</h2>')
+  $(".movies").append("<h2>Nothing added yet</h2>");
 }
-
 
 // const index = myArray.indexOf(2);
 
